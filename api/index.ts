c7bc095c010df0e2bdc355926cf3f0ce2a81940e
thoainv <thoainv@volio.vn>
2024-04-16 17:31:35 +0700

    deploy vercel

---
+++ api/index.ts
@@ -0,0 +1,32 @@
+import routes from "../src/routes";
+import * as bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "../src/models/mongooseDB";
+const express = require("express");
+const app = express();
+import connectSocket from "../src/controllers/socket/socket-io";
+import crawlerWeb from "../src/helpers/crawler";
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+connectDB();
+// crawlerWeb();
+
+// connectSocket();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
